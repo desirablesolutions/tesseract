6bed4297568c76343691dccd14a40a79e31b1013
Jeffrey J. Desir <devops@informandinspire.llc>
2023-05-01 21:31:11 -0400

    @

---
+++ lib/includes/NavBar/react/dist/views/NavBar.d.ts
@@ -0,0 +1,3 @@
+import type { JSXComponentType } from "blakprint/dist/typings";
+import type { NavBarProps } from "src/types";
+export default function NavBar(props: NavBarProps): JSXComponentType<NavBarProps>;
--- lib/includes/NavBar/react/src/views/presets/index.ts
+++ lib/includes/NavBar/react/src/views/presets/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./Larry.tsx"
+export { default } from "./Larry"
