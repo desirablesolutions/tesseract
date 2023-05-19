0e2852284249ca5baed06aef7e659d893b2868b9
Jeffrey Desir <jeffrey@desir.tech>
2023-05-18 22:15:08 -0400

    e

--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import NavBar from "t4-navbar-ts"
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
